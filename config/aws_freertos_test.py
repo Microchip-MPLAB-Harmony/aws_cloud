3c6edfd83c4ec7b7b3e2f7a7ba5ab766147d3847
Shanmugasundaram Viswanathan <shanmugasundaram.viswanathan@microchip.com>
2020-01-23 10:59:14 +0530

    Updated Python logic for new MHC UI Options for Amazon Device Tester MH3-28552.

--- config/aws_freertos_test.py
+++ config/aws_freertos_test.py
@@ -23,17 +23,39 @@
 *****************************************************************************"""
 
 import os
+import time
 from xml.sax.saxutils import quoteattr as xml_quoteattr
 import xml.etree.ElementTree as ET
 
+CONFIG_NAME="config_name"
+CONFIG_TYPE="config_type"
+CONFIG_LABEL="config_label"
+CONFIG_VISIBLE="config_visible"
+CONFIG_DESC="config_descr"
+CONFIG_DEFAULT_VAL="config_default"
+
 coreArch     = Database.getSymbolValue("core", "CoreArchitecture")
 coreFamily   = ATDF.getNode( "/avr-tools-device-file/devices/device" ).getAttribute( "family" )
-autoConnectTableDebug = [["sys_debug", "sys_debug_SYS_CONSOLE_dependency", "sys_console"]]
-autoConnectTableCmd = [["sys_command", "sys_command_SYS_CONSOLE_dependency", "sys_console","sys_console_0"]]
-
 AMAZON_FREERTOS_PATH="../../../../../../../../../../"
 
+CONFIG_FREERTOS="FreeRTOS"
+CONFIG_MICRIUM="MicriumOSIII"
+CONFIG_DEVTEST="AmazonDeviceTester"
 
+CONFIG_COMMON_AFR="config/DT/common_afr.xml"
+CONFIG_UI_COMMON_AFR="config/UI/aws_config_only.xml"
+
+XML_ATTRIB_NAME = "name"
+XML_ATTRIB_DIR  = "dir"
+XML_ATTRIB_FILE  = "file"
+XML_ATTRIB_TEMPLATE = "template"
+XML_ATTRIB_COMPONENTS="components"
+XML_ATTRIB_COMPONENT="component"
+XML_ATTRIB_MENU="menu"
+XML_ATTRIB_COMBO="combo"
+XML_ATTRIB_STRING="string"
+XML_ATTRIB_BOOL="bool"
+XML_ATTRIB_INC="include-files"
 AFR_COMMON_INC_DIR = ("../../../../../../../freertos_kernel/include;"
 "../../../../../../../libraries/c_sdk/standard/common/include;"
 "../../../../../../../libraries/freertos_plus/standard/crypto/include;"
@@ -121,80 +143,34 @@ def deactivateActiveRtos():
     activeComponents = Database.getActiveComponentIDs()
 
     for i in range(0, len(activeComponents)):
-        if (activeComponents[i] == "FreeRTOS"):
-            res = Database.deactivateComponents(["FreeRTOS"])
-        if (activeComponents[i] == "MicriumOSIII"):
-            res = Database.deactivateComponents(["MicriumOSIII"])
-        if (activeComponents[i] == "AmazonFreeRTOS"):
-            res = Database.deactivateComponents(["AmazonFreeRTOS"])
+        if (activeComponents[i] == CONFIG_FREERTOS):
+            res = Database.deactivateComponents([CONFIG_FREERTOS])
+        if (activeComponents[i] == CONFIG_MICRIUM):
+            res = Database.deactivateComponents([CONFIG_MICRIUM])
+        if (activeComponents[i] == CONFIG_DEVTEST):
+            res = Database.deactivateComponents([CONFIG_DEVTEST])
 
 
-def freeRtosIntConfig():
-    if (coreArch == "MIPS"):
-        Timer1InterruptHandlerIndex     = Interrupt.getInterruptIndex("TIMER_1")
 
-        Timer1InterruptEnable           = "EVIC_"+ str(Timer1InterruptHandlerIndex) +"_ENABLE"
+def AddFileTemplate(component, strPath, strFileName, strDestPath,strProjectPath,strType="SOURCE",bMarkup=True):
+    configName = Variables.get("__CONFIGURATION_NAME")
 
-        if (Database.getSymbolValue("core", Timer1InterruptEnable) == False):
-            Database.clearSymbolValue("core", Timer1InterruptEnable)
-            Database.setSymbolValue("core", Timer1InterruptEnable, True)
-
-        #Enable TMR1 Peripheral Clock for FreeRTOS Tick Interrupt Generation
-        if (Database.getSymbolValue("core", "TMR1_CLOCK_ENABLE") == False):
-            Database.clearSymbolValue("core", "TMR1_CLOCK_ENABLE")
-            Database.setSymbolValue("core", "TMR1_CLOCK_ENABLE", True)
-
-    else:
-        SysTickInterruptEnable      = "SysTick_INTERRUPT_ENABLE"
-        SysTickInterruptHandler     = "SysTick_INTERRUPT_HANDLER"
-        SysTickInterruptHandlerLock = "SysTick_INTERRUPT_HANDLER_LOCK"
-
-        if (Database.getSymbolValue("core", SysTickInterruptEnable) == False):
-            Database.clearSymbolValue("core", SysTickInterruptEnable)
-            Database.setSymbolValue("core", SysTickInterruptEnable, True)
-
-        if (Database.getSymbolValue("core", SysTickInterruptHandler) != "xPortSysTickHandler"):
-            Database.clearSymbolValue("core", SysTickInterruptHandler)
-            Database.setSymbolValue("core", SysTickInterruptHandler, "xPortSysTickHandler")
-
-        if (Database.getSymbolValue("core", SysTickInterruptHandlerLock) == False):
-            Database.clearSymbolValue("core", SysTickInterruptHandlerLock)
-            Database.setSymbolValue("core", SysTickInterruptHandlerLock, True)
-
-        PendSVInterruptEnable       = "PendSV_INTERRUPT_ENABLE"
-        PendSVInterruptHandler      = "PendSV_INTERRUPT_HANDLER"
-        PendSVInterruptHandlerLock  = "PendSV_INTERRUPT_HANDLER_LOCK"
-
-        if (Database.getSymbolValue("core", PendSVInterruptEnable) == False):
-            Database.clearSymbolValue("core", PendSVInterruptEnable)
-            Database.setSymbolValue("core", PendSVInterruptEnable, True)
-
-        if (Database.getSymbolValue("core", PendSVInterruptHandler) != "xPortPendSVHandler"):
-            Database.clearSymbolValue("core", PendSVInterruptHandler)
-            Database.setSymbolValue("core", PendSVInterruptHandler, "xPortPendSVHandler")
-
-        if (Database.getSymbolValue("core", PendSVInterruptHandlerLock) == False):
-            Database.clearSymbolValue("core", PendSVInterruptHandlerLock)
-            Database.setSymbolValue("core", PendSVInterruptHandlerLock, True)
-
-        SVCallInterruptEnable       = "SVCall_INTERRUPT_ENABLE"
-        SVCallInterruptHandler      = "SVCall_INTERRUPT_HANDLER"
-        SVCallInterruptHandlerLock  = "SVCall_INTERRUPT_HANDLER_LOCK"
-
-        if (Database.getSymbolValue("core", SVCallInterruptEnable) == False):
-            Database.clearSymbolValue("core", SVCallInterruptEnable)
-            Database.setSymbolValue("core", SVCallInterruptEnable, True)
-
-        if (Database.getSymbolValue("core", SVCallInterruptHandler) != "vPortSVCHandler"):
-            Database.clearSymbolValue("core", SVCallInterruptHandler)
-            Database.setSymbolValue("core", SVCallInterruptHandler, "vPortSVCHandler")
-
-        if (Database.getSymbolValue("core", SVCallInterruptHandlerLock) == False):
-            Database.clearSymbolValue("core", SVCallInterruptHandlerLock)
-            Database.setSymbolValue("core", SVCallInterruptHandlerLock, True)
+    freeRtosAddFile = component.createFileSymbol(strPath.upper(), None)
+    freeRtosAddFile.setSourcePath("templates/demos/" + strFileName)
+    
+    if(strFileName.endswith(".ftl")):
+       strFileName= strFileName[:-4]
+       Log.writeInfoMessage(strPath.upper())
+       Log.writeInfoMessage(strFileName.upper())
+       Log.writeInfoMessage(strDestPath.upper())
+       Log.writeInfoMessage(strProjectPath.upper())
+    freeRtosAddFile.setOutputName(strFileName)
+    freeRtosAddFile.setDestPath(strDestPath)
+    freeRtosAddFile.setProjectPath(strProjectPath)
+    freeRtosAddFile.setType(strType)
+    freeRtosAddFile.setMarkup(bMarkup)
 
 
-   
 def AddFile(component, strPath, strFileName, strDestPath,strProjectPath,strType="SOURCE",bMarkup=False):
     configName = Variables.get("__CONFIGURATION_NAME")
     Log.writeInfoMessage(strPath.upper())
@@ -210,29 +186,83 @@ def AddFile(component, strPath, strFileName, strDestPath,strProjectPath,strType=
     freeRtosAddFile.setMarkup(bMarkup)
 
 
-
 def AddDir(root,component,strPath, strRelativeFilePath,strProjectPath):
     for child in root:
-        if child.tag == "dir":
-            NewstrPath = strPath + "/" + str(child.attrib["name"])
-            NewstrRelativeFilePath = strRelativeFilePath +  "/" + str(child.attrib["name"])
-            NewstrProjectPath = strProjectPath +  "/" + str(child.attrib["name"])
+        if child.tag == XML_ATTRIB_DIR:
+            NewstrPath = strPath + "/" + str(child.attrib[XML_ATTRIB_NAME])
+            NewstrRelativeFilePath = strRelativeFilePath +  "/" + str(child.attrib[XML_ATTRIB_NAME])
+            NewstrProjectPath = strProjectPath +  "/" + str(child.attrib[XML_ATTRIB_NAME])
             AddDir(child,component,NewstrPath, NewstrRelativeFilePath ,NewstrProjectPath)
-        if child.tag == "file":
-            NewstrPath = strPath + "/" + str(child.attrib["name"])
-            AddFile(component,NewstrPath, str(child.attrib["name"]), strRelativeFilePath ,strProjectPath)
+        if child.tag == XML_ATTRIB_FILE:
+            NewstrPath = strPath + "/" + str(child.attrib[XML_ATTRIB_NAME])
+            AddFile(component,NewstrPath, str(child.attrib[XML_ATTRIB_NAME]), strRelativeFilePath ,strProjectPath)
+
+        elif child.tag == XML_ATTRIB_TEMPLATE:
+            NewstrPath = strPath + "/" + str(child.attrib[XML_ATTRIB_NAME])
+            AddFileTemplate(component,NewstrPath, str(child.attrib[XML_ATTRIB_NAME]), strRelativeFilePath ,strProjectPath)
+
+
 
 def AddAWSFile(component,strPath, strRelativeFilePath, strXmlFile):
     tree = ET.parse(strXmlFile)
     root = tree.getroot()
     for child in root:
-        if child.tag == "dir":
-            AddDir(child,component,strPath + "/" + str(child.attrib["name"]), AMAZON_FREERTOS_PATH + strRelativeFilePath +  "/" + str(child.attrib["name"]),strRelativeFilePath +  "/" + str(child.attrib["name"]))
-        if child.tag == "file":
-            AddFile(component,strPath + "/" + str(child.attrib["name"]), str(child.attrib["name"]), strRelativeFilePath+ "/" + str(child.attrib["name"]),strRelativeFilePath + "/" + str(child.attrib["name"]))
+        if child.tag == XML_ATTRIB_DIR:
+            AddDir(child,component,strPath + "/" + str(child.attrib[XML_ATTRIB_NAME]), AMAZON_FREERTOS_PATH + strRelativeFilePath +  "/" + str(child.attrib[XML_ATTRIB_NAME]),strRelativeFilePath +  "/" + str(child.attrib[XML_ATTRIB_NAME]))
+        if child.tag == XML_ATTRIB_FILE:
+            AddFile(component,strPath + "/" + str(child.attrib[XML_ATTRIB_NAME]), str(child.attrib[XML_ATTRIB_NAME]), strRelativeFilePath+ "/" + str(child.attrib[XML_ATTRIB_NAME]),strRelativeFilePath + "/" + str(child.attrib[XML_ATTRIB_NAME]))
+        if child.tag == XML_ATTRIB_TEMPLATE:
+            AddFileTemplate(component,strPath + "/" + str(child.attrib[XML_ATTRIB_NAME]), str(child.attrib[XML_ATTRIB_NAME]), strRelativeFilePath+ "/" + str(child.attrib[XML_ATTRIB_NAME]),strRelativeFilePath + "/" + str(child.attrib[XML_ATTRIB_NAME]))
+            
         
-   
-#Instatntiate AmazonFreeRTOS Device Tester Component
+
+def AddAWSConfig(aws_cloud,strXmlFile):
+    tree = ET.parse(strXmlFile)
+    root = tree.getroot()
+    for child in root:
+        if child.tag == XML_ATTRIB_COMPONENTS:
+            temp_elem = AddAWSConfiguration(aws_cloud,None,child.attrib[CONFIG_NAME],child.attrib[CONFIG_TYPE],child.attrib[CONFIG_LABEL], child.attrib[CONFIG_DESC], child.attrib[CONFIG_DEFAULT_VAL], child.attrib[CONFIG_VISIBLE])
+            AddAWSComponent(aws_cloud,child,temp_elem,child.attrib[CONFIG_NAME],child.attrib[CONFIG_TYPE],child.attrib[CONFIG_LABEL], child.attrib[CONFIG_DESC], child.attrib[CONFIG_DEFAULT_VAL], child.attrib[CONFIG_VISIBLE])
+        if child.tag == XML_ATTRIB_COMPONENT:
+            AddAWSConfiguration(aws_cloud,None,child.attrib[CONFIG_NAME],child.attrib[CONFIG_TYPE],child.attrib[CONFIG_LABEL], child.attrib[CONFIG_DESC], child.attrib[CONFIG_DEFAULT_VAL], child.attrib[CONFIG_VISIBLE])
+
+
+def AddAWSComponent(aws_cloud,root,parent_config,config_name,config_type,strLabel,strDesc,strDefaultValue,bVisible):
+    for child in root:
+        if child.tag == XML_ATTRIB_COMPONENTS:
+            AddAWSComponent(aws_cloud,parent_config,child.attrib[CONFIG_NAME],child.attrib[CONFIG_TYPE],child.attrib[CONFIG_LABEL], child.attrib[CONFIG_DESC], child.attrib[CONFIG_DEFAULT_VAL], child.attrib[CONFIG_VISIBLE])
+        if child.tag == XML_ATTRIB_COMPONENT:
+            AddAWSConfiguration(aws_cloud,parent_config,child.attrib[CONFIG_NAME],child.attrib[CONFIG_TYPE],child.attrib[CONFIG_LABEL], child.attrib[CONFIG_DESC], child.attrib[CONFIG_DEFAULT_VAL], child.attrib[CONFIG_VISIBLE])
+
+def AddAWSConfiguration(aws_cloud,parent_config,config_name,config_type,strLabel,strDesc,strDefaultValue,bVisible):
+    if str(config_type).lower() == XML_ATTRIB_MENU:
+        freeRtosSymMenu = aws_cloud.createMenuSymbol(config_name, parent_config)
+    elif str(config_type).lower() == XML_ATTRIB_STRING:
+        freeRtosSymMenu = aws_cloud.createStringSymbol(config_name, parent_config)
+        freeRtosSymMenu.setDefaultValue(str(strDefaultValue))
+    elif str(config_type).lower() == XML_ATTRIB_BOOL:
+        freeRtosSymMenu = aws_cloud.createBooleanSymbol(config_name, parent_config)
+        freeRtosSymMenu.setDefaultValue(bool(strDefaultValue))
+    elif str(config_type).lower() == XML_ATTRIB_COMBO:
+        strValues = strDefaultValue.split(' ')
+        freeRtosSymMenu = aws_cloud.createComboSymbol(config_name, parent_config,strValues)
+        freeRtosSymMenu.setDefaultValue("SHADOW")
+    else: #if type.lower() == "integer": considered integer.
+        freeRtosSymMenu = aws_cloud.createIntegerSymbol(config_name, parent_config)
+        freeRtosSymMenu.setDefaultValue(int(strDefaultValue))
+        freeRtosSymMenu.setMin(0)
+        freeRtosSymMenu.setMax(999999999)
+	
+    freeRtosSymMenu.setLabel(strLabel)
+    freeRtosSymMenu.setDescription(strDesc)
+    freeRtosSymMenu.setVisible(False)
+	
+    #How to set generic Default Value.
+    if(str(bVisible).lower()=="true"):
+        freeRtosSymMenu.setVisible(True)
+    return freeRtosSymMenu
+
+#Instatntiate FreeRTOS Component
 def instantiateComponent(aws_cloud):
     Log.writeInfoMessage("Running AmazonFreeRTOS Device Tester")
 
@@ -240,30 +270,19 @@ def instantiateComponent(aws_cloud):
     deactivateActiveRtos()
     autoComponentIDTable = ["stdio"]
     res = Database.activateComponents(autoComponentIDTable)
-    if(Database.getComponentByID("sys_debug") == None):
-        res = Database.activateComponents(["sys_debug"])
-        res = Database.connectDependencies(autoConnectTableDebug)    
-    if(Database.getComponentByID("sys_command") == None):
-        res = Database.activateComponents(["sys_command"])
-        res = Database.connectDependencies(autoConnectTableCmd)  
+    #AmazonFreeRTOS Configuration Menu
+    
+    AddAWSConfig(aws_cloud,Module.getPath()+ CONFIG_UI_COMMON_AFR)
+    configName = Variables.get("__CONFIGURATION_NAME")
 
-    if ((coreArch == "CORTEX-M4") or (coreArch == "CORTEX-M7")):
-        if(Database.getComponentByID("trng") == None):
-            res = Database.activateComponents(["trng"])
+    if (coreArch == "MIPS"):
+        AddMIPS(aws_cloud,configName)
+    elif (coreArch == "CORTEX-M4"):
+        AddSAME54(aws_cloud,configName)
+    elif (coreArch == "CORTEX-M7"):
+        AddSAME70(aws_cloud,configName)
 
-    if ((coreArch == "MIPS")):
-        if(Database.getComponentByID("nvm") == None):
-            res = Database.activateComponents(["nvm"])
-        if(Database.getComponentByID("rng") == None):
-            res = Database.activateComponents(["rng"])    
 
-    freeRtosIntConfig()
-    Database.setSymbolValue("HarmonyCore", "SELECT_RTOS","FreeRTOS")
-
-    #FreeRTOS Configuration Menu
-    freeRtosSymMenu = aws_cloud.createMenuSymbol("AWS_FREERTOS_MENU", None)
-    freeRtosSymMenu.setLabel("AmazonFreeRTOS Configuration")
-    freeRtosSymMenu.setDescription("List of AWS Options")
     Log.writeInfoMessage(Module.getPath())
 	
     awsSystemDefFile = aws_cloud.createFileSymbol("AWS_SYS_DEF_HEADER", None)
@@ -272,26 +291,19 @@ def instantiateComponent(aws_cloud):
     awsSystemDefFile.setSourcePath("templates/definitions.h.ftl")
     awsSystemDefFile.setMarkup(True)
     
+    
 
 ############################################################################
 #### Code Generation ####
 ############################################################################
 
     configName = Variables.get("__CONFIGURATION_NAME")
-    AddAWSFile(aws_cloud,"",  "",Module.getPath()+"config/Test_"+coreArch+".xml")
-    
-    if (coreArch == "MIPS"):
-        AddMIPS(aws_cloud,configName)
-    elif (coreArch == "CORTEX-M4"):
-        AddSAME54(aws_cloud,configName)
-    elif (coreArch == "CORTEX-M7"):
-        AddSAME70(aws_cloud,configName)
-
     freeRtosIncWarn = aws_cloud.createSettingSymbol("AFR_XC32_WARNING", None)
     freeRtosIncWarn.setCategory("C32")
     freeRtosIncWarn.setKey("make-warnings-into-errors")
     freeRtosIncWarn.setValue("false")
-
+    AddAWSFile(aws_cloud,"../",  "",Module.getPath()+CONFIG_COMMON_AFR)
+    
 
 def AddMIPS(aws_cloud,configName):
     freeRtosIncDirForPre = aws_cloud.createSettingSymbol("AFR1_XC32_AS_PRE_PROC_DIRS", None)
