2ada1f5e34795229aa8e28cda8df486fadd5ff60
Shanmugasundaram Viswanathan <shanmugasundaram.viswanathan@microchip.com>
2020-07-17 19:46:48 +0530

    Added LED Shadow Client for WINC3400 + SAME54 + ECC608 (Wifi solution). MH3-29862.

---
+++ apps/led_shadow_client_ecc/firmware/sam_e54_xpro_winc3400.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# sam_e54_xpro_winc3400 configuration
+CND_ARTIFACT_DIR_sam_e54_xpro_winc3400=dist/sam_e54_xpro_winc3400/production
+CND_ARTIFACT_NAME_sam_e54_xpro_winc3400=sam_e54_xpro_winc3400.X.production.hex
+CND_ARTIFACT_PATH_sam_e54_xpro_winc3400=dist/sam_e54_xpro_winc3400/production/sam_e54_xpro_winc3400.X.production.hex
+CND_PACKAGE_DIR_sam_e54_xpro_winc3400=${CND_DISTDIR}/sam_e54_xpro_winc3400/package
+CND_PACKAGE_NAME_sam_e54_xpro_winc3400=same54xprowinc3400.x.tar
+CND_PACKAGE_PATH_sam_e54_xpro_winc3400=${CND_DISTDIR}/sam_e54_xpro_winc3400/package/same54xprowinc3400.x.tar
